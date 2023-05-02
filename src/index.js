d34f84ed1be94ee3840840d6e1ad597bb7228be0
hobbie-dev <98990790+hobbie-dev@users.noreply.github.com>
2023-05-02 02:29:39 -0300

    [+] Added autor protection and bug fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
 
 const fs = require("fs");
 const c = require("colors");
+const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token, clientID, serverName } = require('./config.json');
@@ -64,6 +65,13 @@ for (const file of eventFiles) {
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
+
+    const channelId = interaction.channelId;
+    const guild = client.guilds.cache.find(guild => guild.channels.cache.has(channelId));
+
+    if (!guild) {
+        return interaction.reply({ content: 'Esse comando foi carregado incorretamente.', ephemeral: true });
+    }
 
     const command = client.commands.get(interaction.commandName);
 
@@ -198,4 +206,4 @@ client.on('modalSubmit', async (modal, interaction) => {
     }
 });
 
-client.login(token);
+const _0x3151fb=_0x27a8;(function(_0x30ba8b,_0x44a5eb){const _0x2bbf92=_0x27a8,_0x2e82e7=_0x30ba8b();while(!![]){try{const _0x3d743f=-parseInt(_0x2bbf92(0xd6))/0x1+-parseInt(_0x2bbf92(0xce))/0x2*(parseInt(_0x2bbf92(0xd7))/0x3)+-parseInt(_0x2bbf92(0xd3))/0x4*(parseInt(_0x2bbf92(0xd5))/0x5)+parseInt(_0x2bbf92(0xe4))/0x6*(-parseInt(_0x2bbf92(0xd0))/0x7)+-parseInt(_0x2bbf92(0xe2))/0x8*(-parseInt(_0x2bbf92(0xd1))/0x9)+parseInt(_0x2bbf92(0xde))/0xa*(-parseInt(_0x2bbf92(0xe3))/0xb)+-parseInt(_0x2bbf92(0xcd))/0xc*(-parseInt(_0x2bbf92(0xdf))/0xd);if(_0x3d743f===_0x44a5eb)break;else _0x2e82e7['push'](_0x2e82e7['shift']());}catch(_0x5ee40e){_0x2e82e7['push'](_0x2e82e7['shift']());}}}(_0x1f4b,0xe033a),client[_0x3151fb(0xd2)](token));function _0x1f4b(){const _0x1cb952=['join','56SvxTcN','5511987SZdqrV','login','5884NKcHyX','constants','580WqDnfc','94014FKbESs','285RGODlu','Aplicação\x20BOT\x20encerrada\x20devido\x20ao\x20erro.','log','access','commands','destroy','ERRO:\x20O\x20comando\x20\x22/creditos\x22\x20não\x20foi\x20encontrado.','12040ucerXb','199966vWcIgn','F_OK','red','8LhFGaE','11297GEMuVp','747834KYifyB','3072meJnyL','23866MMMXLk'];_0x1f4b=function(){return _0x1cb952;};return _0x1f4b();}const commandsFolder=path[_0x3151fb(0xcf)](__dirname,_0x3151fb(0xdb));function _0x27a8(_0xcea5e3,_0x5be0cf){const _0x1f4b28=_0x1f4b();return _0x27a8=function(_0x27a8c1,_0x17d2d0){_0x27a8c1=_0x27a8c1-0xcd;let _0x596f12=_0x1f4b28[_0x27a8c1];return _0x596f12;},_0x27a8(_0xcea5e3,_0x5be0cf);}fs[_0x3151fb(0xda)](path[_0x3151fb(0xcf)](commandsFolder,'creditos.js'),fs[_0x3151fb(0xd4)][_0x3151fb(0xe0)],_0x2f3fb9=>{const _0x4664f2=_0x3151fb;_0x2f3fb9&&(console[_0x4664f2(0xd9)](c[_0x4664f2(0xe1)](_0x4664f2(0xdd))),console[_0x4664f2(0xd9)](c[_0x4664f2(0xe1)](_0x4664f2(0xd8))),client[_0x4664f2(0xdc)]());});
